a4779b94732ed53539434cd28b602fb4b3bb33c2
Luqmaan Sulaiman <luqmaansu@gmail.com>
2023-05-13 11:45:09 +0800

    updates

--- app_blog/forms.py
+++ app_blog/forms.py
@@ -1,23 +1,61 @@
 from django import forms
 from django.contrib.auth import get_user_model
 from django.forms import ModelForm, ClearableFileInput
-from .models import Post
+from .models import Post, Comment
+from django.shortcuts import get_object_or_404
 
 class PostForm(ModelForm):
     class Meta:
         model = Post
-        fields = ['title', 'content', 'tags', 'series', 'contributors']
-    
-    files = forms.FileField(widget=ClearableFileInput(attrs={'multiple': True}), required=False)
+        fields = ["title", "content", "tags", "series", "contributors"]
+
+    files = forms.FileField(
+        widget=ClearableFileInput(attrs={"multiple": True}), required=False
+    )
 
     def __init__(self, *args, **kwargs):
-        self.user = kwargs.pop('user', None)
+        self.user = kwargs.pop("user", None)
         super(PostForm, self).__init__(*args, **kwargs)
         if self.user:
             # Exclude the current user from the list of contributors
-            self.fields['contributors'].queryset = get_user_model().objects.exclude(id=self.user.id)
+            self.fields["contributors"].queryset = get_user_model().objects.exclude(
+                id=self.user.id
+            )
 
         super(PostForm, self).__init__(*args, **kwargs)
-        self.fields['tags'].widget.attrs.update({'class': 'form-control'})
-        self.fields['series'].widget.attrs.update({'class': 'form-control'})
-        self.fields['contributors'].widget.attrs.update({'class': 'form-control'})
+        self.fields["tags"].widget.attrs.update({"class": "form-control"})
+        self.fields["series"].widget.attrs.update({"class": "form-control"})
+        self.fields["contributors"].widget.attrs.update({"class": "form-control"})
+
+
+# Define a form class to be used for ReplyCommentCreateView
+class ReplyCommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = (
+            "comment_detail",
+            "commenter",
+        )
+
+    def __init__(self, *args, pk=None, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.pk = pk
+
+    # Clean the form to assign the parent comment and post to the reply comment
+    def clean(self):
+        
+        # Get the comment to be created
+        cleaned_data = super().clean()
+        instance = self.instance
+
+        # Get the pk kwargs from the url
+        pk = self.pk
+
+        # Assign the parent comment to the instance
+        instance.parent = get_object_or_404(Comment, pk=pk)
+
+        # Assign the post to the instance
+        instance.post = instance.parent.post
+
+        # Done
+        return cleaned_data
--- app_blog/views.py
+++ app_blog/views.py
@@ -9,73 +9,83 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import get_object_or_404
 from .forms import PostForm
 
+
 # Create your views here.
 def blog_home(request):
     context = {}
-    context['visitor_name'] = request.user
-    return render(request, 'app_blog/home.html', context=context)
+    context["visitor_name"] = request.user
+    return render(request, "app_blog/home.html", context=context)
+
 
 # Class Based View Model
 # Model Series CRUD
 class SeriesListView(ListView):
     model = Series
-    template_name = 'app_blog/series/series_list.html'
+    template_name = "app_blog/series/series_list.html"
+
 
 class SeriesDetailView(DetailView):
     model = Series
-    template_name = 'app_blog/series/series_detail.html'
-    context_object_name = 'series'
+    template_name = "app_blog/series/series_detail.html"
+    context_object_name = "series"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['post'] = Post.objects.filter(series=self.object)
-        context['x'] = 1
+        context["post"] = Post.objects.filter(series=self.object)
+        context["x"] = 1
         return context
+
 
 class SeriesCreateView(CreateView):
     model = Series
-    template_name = 'app_blog/series/series_form.html'
-    fields = '__all__'
+    template_name = "app_blog/series/series_form.html"
+    fields = "__all__"
     success_url = reverse_lazy("app_blog:series-list")
+
 
 class SeriesUpdateView(UpdateView):
     model = Series
-    template_name = 'app_blog/series/series_update.html'
+    template_name = "app_blog/series/series_update.html"
     fields = "__all__"
-    success_url = reverse_lazy("app_blog:series-list") # when a Post is successfully updated, redirect to this url
+    success_url = reverse_lazy(
+        "app_blog:series-list"
+    )  # when a Post is successfully updated, redirect to this url
+
 
 class SeriesDeleteView(DeleteView):
     model = Series
-    template_name = 'app_blog/series/series_confirm_delete.html'
+    template_name = "app_blog/series/series_confirm_delete.html"
     success_url = reverse_lazy("app_blog:series-list")
+
 
 # Model Post CRUD
 class PostListView(ListView):
     model = Post
-    template_name = 'app_blog/post/post_list.html'
+    template_name = "app_blog/post/post_list.html"
+
 
 class PostDetailView(DetailView):
     model = Post
-    template_name = 'app_blog/post/post_detail.html'
+    template_name = "app_blog/post/post_detail.html"
 
-    
+
 class PostCreateView(CreateView):
     model = Post
     form_class = PostForm
-    template_name = 'app_blog/post/post_form.html'
+    template_name = "app_blog/post/post_form.html"
     success_url = reverse_lazy("app_blog:post-list")
 
     def form_valid(self, form):
         post = form.save(commit=False)
         post.author = self.request.user
         post.save()
-        for f in self.request.FILES.getlist('files'):
+        for f in self.request.FILES.getlist("files"):
             PostFile.objects.create(post=post, file=f)
         return super().form_valid(form)
-    
+
     def get_form_kwargs(self):
         kwargs = super().get_form_kwargs()
-        kwargs['user'] = self.request.user
+        kwargs["user"] = self.request.user
         return kwargs
 
     # Behind the scenes,
@@ -84,119 +94,143 @@ class PostCreateView(CreateView):
     # Default server-side validation
     # Default client-side validation
 
+
 class PostUpdateView(UpdateView):
     model = Post
     fields = "__all__"
-    template_name = 'app_blog/post/post_update.html'
-    success_url = reverse_lazy("app_blog:post-list") # when a Post is successfully updated, redirect to this url
+    template_name = "app_blog/post/post_update.html"
+    success_url = reverse_lazy(
+        "app_blog:post-list"
+    )  # when a Post is successfully updated, redirect to this url
 
 
 class PostDeleteView(DeleteView):
     model = Post
-    template_name = 'app_blog/post/post_confirm_delete.html'
+    template_name = "app_blog/post/post_confirm_delete.html"
     success_url = reverse_lazy("app_blog:post-list")
+
 
 # Model Comment CRUD
 class CommmentAddView(LoginRequiredMixin, CreateView):
     model = Comment
-    fields = ['comment_detail']
-    template_name = 'app_blog/comment/comment_form.html'
+    fields = ["comment_detail"]
+    template_name = "app_blog/comment/comment_form.html"
     success_url = reverse_lazy("app_blog:comment-list")
 
     def form_valid(self, form):
-        post_id = get_object_or_404(Post, pk=self.kwargs['pk'])
+        post_id = get_object_or_404(Post, pk=self.kwargs["pk"])
         form.instance.post = post_id
         form.instance.commenter = self.request.user
 
         return super().form_valid(form)
-                
-def replyComment(request,pk):
-   parent_comment = Comment.objects.get(id= pk)
-   
-   if request.method == 'POST':
-       replier_name = request.user
-       reply_content = request.POST.get('reply_content')
-       postid = request.POST.get('postID')
-       post = Post.objects.get(id= postid)
-       newReply = Comment(commenter=replier_name, comment_detail=reply_content, post=post, parent = parent_comment)
-       newReply.save()
-       return redirect('app_blog:comment-list')
-   
 
-class ReplyCommentCreateView(LoginRequiredMixin, CreateView):
 
+def replyComment(request, pk):
+    parent_comment = Comment.objects.get(id=pk)
+
+    if request.method == "POST":
+        replier_name = request.user
+        reply_content = request.POST.get("reply_content")
+        postid = request.POST.get("postID")
+        post = Post.objects.get(id=postid)
+        newReply = Comment(
+            commenter=replier_name,
+            comment_detail=reply_content,
+            post=post,
+            parent=parent_comment,
+        )
+        newReply.save()
+        return redirect("app_blog:comment-list")
+
+
+from .forms import ReplyCommentForm
+
+class GetURLPKMixin:
+    def get_form_kwargs(self):
+        kwargs = super().get_form_kwargs()
+        kwargs["pk"] = self.kwargs.get("pk")
+        return kwargs
+
+class ReplyCommentCreateView(LoginRequiredMixin, GetURLPKMixin, CreateView):
     model = Comment
-    fields = ('comment_detail', 'commenter',)
-    template_name = 'app_blog/comment/reply_comment_form.html'
+    form_class = ReplyCommentForm
+    template_name = "app_blog/comment/reply_comment_form.html"
     success_url = reverse_lazy("app_blog:comment-list")
 
-    # Ensure that a comment with the url pk exists by overriding get method
+    # Ensure that a comment with the url pk exists when a user visits the page
     def get(self, request, *args, **kwargs):
-
-        comment = get_object_or_404(Comment, pk=self.kwargs['pk'])
+        comment = get_object_or_404(Comment, pk=self.kwargs["pk"])
         return super().get(request, *args, **kwargs)
 
     # Assign the parent comment and post to the reply comment
-    def form_valid(self, form):
+    # def form_valid(self, form):
 
-        # Get the comment to be created
-        instance = form.instance
+    #     # Get the comment to be created
+    #     instance = form.instance
 
-        # Assign the parent comment to the instance
-        instance.parent = get_object_or_404(Comment, pk=self.kwargs['pk'])
+    #     # Assign the parent comment to the instance
+    #     instance.parent = get_object_or_404(Comment, pk=self.kwargs['pk'])
 
-        # Assign the post to the instance
-        instance.post = instance.parent.post
+    #     # Assign the post to the instance
+    #     instance.post = instance.parent.post
 
-        # Done
-        return super().form_valid(form)
+    #     # Done
+    #     return super().form_valid(form)
 
-   
+
 class CommentUpdateView(UpdateView):
     model = Comment
-    fields = ['comment_detail']
-    template_name = 'app_blog/comment/comment_update.html'
-    success_url = reverse_lazy("app_blog:comment-list") # when a Post is successfully updated, redirect to this url
+    fields = ["comment_detail"]
+    template_name = "app_blog/comment/comment_update.html"
+    success_url = reverse_lazy(
+        "app_blog:comment-list"
+    )  # when a Post is successfully updated, redirect to this url
+
 
 class CommentDeleteView(DeleteView):
     model = Comment
-    template_name = 'app_blog/comment/comment_confirm_delete.html'
+    template_name = "app_blog/comment/comment_confirm_delete.html"
     success_url = reverse_lazy("app_blog:comment-list")
+
 
 class CommentListView(ListView):
     model = Comment
-    template_name = 'app_blog/comment/comment_list.html'
+    template_name = "app_blog/comment/comment_list.html"
+
 
 class CommentDetailView(DetailView):
     model = Comment
-    template_name = 'app_blog/comment/comment_detail.html'
+    template_name = "app_blog/comment/comment_detail.html"
+
 
 # Model Tag CRUD
 class TagListView(ListView):
     model = Tag
-    template_name = 'app_blog/tag/tag_list.html'
+    template_name = "app_blog/tag/tag_list.html"
+
 
 class TagDetailView(DetailView):
     model = Tag
-    template_name = 'app_blog/tag/tag_detail.html'
+    template_name = "app_blog/tag/tag_detail.html"
+
 
 class TagCreateView(CreateView):
     model = Tag
-    fields = '__all__'
-    template_name = 'app_blog/tag/tag_form.html'
+    fields = "__all__"
+    template_name = "app_blog/tag/tag_form.html"
     success_url = reverse_lazy("app_blog:tag-list")
+
 
 class TagUpdateView(UpdateView):
     model = Tag
     fields = "__all__"
-    template_name = 'app_blog/tag/tag_update.html'
-    success_url = reverse_lazy("app_blog:tag-list") # when a Post is successfully updated, redirect to this url
+    template_name = "app_blog/tag/tag_update.html"
+    success_url = reverse_lazy(
+        "app_blog:tag-list"
+    )  # when a Post is successfully updated, redirect to this url
+
 
 class TagDeleteView(DeleteView):
     model = Tag
-    template_name = 'app_blog/tag/tag_confirm_delete.html'
+    template_name = "app_blog/tag/tag_confirm_delete.html"
     success_url = reverse_lazy("app_blog:tag-list")
-
-
-
-
