a54995e644b1cdcfd7549a308899f7911e3f7d29
Luqmaan Sulaiman <luqmaansu@gmail.com>
2023-05-13 11:46:58 +0800

    update

--- app_blog/models.py
+++ app_blog/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.conf import settings
 
 class Tag(models.Model):
-    name = models.CharField(max_length=50, default="")
+    name = models.CharField(max_length=50, default="", unique=True)
 
     def __str__(self):
         return self.name
